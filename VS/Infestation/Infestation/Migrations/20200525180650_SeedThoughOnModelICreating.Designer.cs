9c5840da95f6660a7cfe10f608028291899737ae
MedJay00 <54895669+MedJay00@users.noreply.github.com>
2020-05-25 21:24:12 +0300

    DZ_3,DZ_4 and fixed DZ_2

--- VS/Infestation/Infestation/Migrations/20200525180650_SeedThoughOnModelICreating.Designer.cs
+++ VS/Infestation/Infestation/Migrations/20200525180650_SeedThoughOnModelICreating.Designer.cs
@@ -3,14 +3,16 @@ using Infestation.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infestation.Migrations
 {
     [DbContext(typeof(InfestationContext))]
-    partial class InfestationContextModelSnapshot : ModelSnapshot
+    [Migration("20200525180650_SeedThoughOnModelICreating")]
+    partial class SeedThoughOnModelICreating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
